b3a6b8eec60b6017f54c93cc976ded35bcdb6b58
meishuqin <18665056978@163.com>
2019-08-03 16:21:29 +0800

    计划任务独立

--- application/chuangke/controller/Index.php
+++ application/chuangke/controller/Index.php
@@ -63,32 +63,4 @@ class Index extends MobileBase
 
         return $this->fetch();
     }
-    /**
-     * 自动审核计划任务
-     */
-    public function autoCheck(){
-        $shop_info = tpCache('shop_info');
-        $time = time() - $shop_info['autoTime'] * 60;
-        $where = "status=1 and createtime<$time and " .
-            "(moneys <= ".$shop_info['artificial_min1']." and type=1 or ".
-            "moneys <= ". $shop_info['artificial_min2']." and type=2 or ".
-            "moneys <= ". $shop_info['artificial_min3']." and type=3 or ".
-            "moneys <= ". $shop_info['artificial_min4']." and type=4)";
-
-        $list = M('user_debt')->where(['status' => 1,'createtime' => ['LT',$time]])->select();
-        foreach ($list as $key => $value) {
-            if ($value['moneys'] > $shop_info['artificial_min'.$value['type']]) continue;
-            // 更新会员等级
-            $res = M('user_debt')->where(['id' => $value['id']])->update(['status' => 2,'update_time' =>time()]);
-            $mobile = M('users')->where(['user_id' => $value['user_id']])->value('mobile');
-
-            if ($res) {
-                // 发送短信
-                $msg = jh_message($mobile,176933,'');
-            }
-        }
-        // $data = M('user_debt')->where($where)->select();
-        // M('user_debt')->where($where)->update(['status' => 2,'update_time' =>time()]);
-    }
-
 }
