2672a399e47fefdf2a9d2c9376c53070fada67bb
meishuqin <18665056978@163.com>
2019-08-05 15:56:50 +0800

    1

--- application/chuangke/controller/Plan.php
+++ application/chuangke/controller/Plan.php
@@ -112,7 +112,7 @@ class Plan extends MobileBase
     public function add_debt(){
 
         if($this->request->isPost()){
-            $user = M('users')->field('is_lock')->where(['user_id' => $this->user_id])->find();
+            $user = M('users')->field('is_lock,level')->where(['user_id' => $this->user_id])->find();
             if ($user['is_lock'] == 1) $this->error('账号已被冻结,请联系管理员');
             $shop_info = tpCache('shop_info');
             $text = $shop_info['shop_text'];
@@ -122,7 +122,7 @@ class Plan extends MobileBase
             if (!$is_authent) $this->ajaxReturn(['status'=>0,'msg'=>$text.'：请先在个人中心实名认证！','url' => U('chuangke/Member/realNameAuthentication')]);
             // 是否绑定收款方式
             $is_receivables = M('receipt_information')->where(['user_id' => $this->user_id])->count();
-            if (!$is_receivables) $this->ajaxReturn(['status'=>0,'msg'=>$text.'：请先在个人中心绑定收款方式！','url' => U('chuangke/Member/paymentMethod')]);
+            // if (!$is_receivables) $this->ajaxReturn(['status'=>0,'msg'=>$text.'：请先在个人中心绑定收款方式！','url' => U('chuangke/Member/paymentMethod')]);
 
             $money = input('post.money');
             $type = input('post.type');
