00308c0c31f06165efdd3f724d5d8854a123a015
zhuoxuezhong <1062763849@qq.com>
2019-08-07 19:28:42 +0800

    下载图片

--- application/chuangke/controller/Member.php
+++ application/chuangke/controller/Member.php
@@ -227,7 +227,7 @@ class Member extends  MobileBase
         $data['url'] = $url;
         $data['img'] = $img;
         $data['code'] = $code;
-
+        
         $this->assign('user',$user);
         $this->assign('data',$data);
 
