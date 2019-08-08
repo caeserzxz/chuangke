806f51f0031f96c1a77d06f05ca2f5b9b81301f5
zhuoxuezhong <1062763849@qq.com>
2019-08-08 17:21:44 +0800

    下载图片

--- application/chuangke/controller/Member.php
+++ application/chuangke/controller/Member.php
@@ -221,7 +221,6 @@ class Member extends  MobileBase
             $qr::png($url,'./'.$after_path, "M", 6,TRUE);
         }
         $img = request()->domain().'/'.$after_path;
-
         $code = M('tuijian_code')->where(['user_id' => $user['user_id']])->value('code');
 
         $data['url'] = $url;
@@ -388,4 +387,169 @@ class Member extends  MobileBase
             $this->ajaxReturn(['status' => -1, 'msg' => '修改微信失败']);
         }
     }
+
+
+
+    public  function huahua(){
+        $im = './public/static/chuangke/images/shareBG.png';
+        $erweima = I('erweima');
+        $wx_code = I('wx_code');
+            $config = array(
+                'image'=>array(
+                    array(
+                        'url'=>"$erweima",     //二维码资源
+                        'stream'=>0,
+                        'left'=>240,
+                        'top'=>500,
+                        'right'=>0,
+                        'bottom'=>0,
+                        'width'=>280,
+                        'height'=>280,
+                        'opacity'=>100
+                    )
+                ),
+                'text'=> array(
+                    array(
+                        'text'=>"$wx_code",
+                        'left'=>190,
+                        'top'=>850,
+                        'fontSize'=>30,       //字号
+                        'fontColor'=>'255,240,245', //字体颜色
+                        'angle'=>0,
+                    )
+                ),
+                'background'=>$im,         //背景图
+            );
+        $filename = 'public/qrcode/'.time().'.png';
+        return  $this->createPoster($config,$filename);
+        exit;
+    }
+
+
+    function createPoster($config=array(),$filename=""){
+//        header("Content-type: text/html; charset=utf-8");
+        //如果要看报什么错，可以先注释调这个header
+//        if(empty($filename))
+            header("content-type: image/png");
+//        $imageDefault = array(
+//            'left'=>0,
+//            'top'=>0,
+//            'right'=>0,
+//            'bottom'=>0,
+//            'width'=>100,
+//            'height'=>100,
+//            'opacity'=>100
+//        );
+//        $textDefault = array(
+//            'left'=>0,
+//            'top'=>0,
+//            'fontSize'=>32,       //字号
+//            'fontColor'=>'0,0,0', //字体颜色
+//            'angle'=>0,
+//        );
+
+        $background = $config['background'];//海报最底层得背景
+        //背景方法
+        $backgroundInfo = getimagesize($background);
+        $backgroundFun = 'imagecreatefrom'.image_type_to_extension($backgroundInfo[2], false);
+        $background = $backgroundFun($background);
+        $backgroundWidth = imagesx($background);  //背景宽度
+        $backgroundHeight = imagesy($background);  //背景高度
+        $imageRes = imageCreatetruecolor($backgroundWidth,$backgroundHeight);
+        $color = imagecolorallocate($imageRes, 255, 255, 255);
+        imagefill($imageRes, 0, 0, $color);
+        // imageColorTransparent($imageRes, $color);  //颜色透明
+        imagecopyresampled($imageRes,$background,0,0,0,0,imagesx($background),imagesy($background),imagesx($background),imagesy($background));
+
+        if(!empty($config['image'])){
+            foreach ($config['image'] as $key => $val) {
+//                $val = array_merge($imageDefault,$val);
+                $info = getimagesize($val['url']);
+                $function = 'imagecreatefrom'.image_type_to_extension($info[2], false);
+
+                if($val['stream']){   //如果传的是字符串图像流
+                    $info = getimagesizefromstring($val['url']);
+                    $function = 'imagecreatefromstring';
+                }
+                $res = $function($val['url']);
+                $resWidth = $info[0];
+                $resHeight = $info[1];
+                //建立画板 ，缩放图片至指定尺寸
+
+                $canvas=imagecreatetruecolor($val['width'], $val['height']);
+                imagefill($canvas, 0, 0, $color);
+                //关键函数，参数（目标资源，源，目标资源的开始坐标x,y, 源资源的开始坐标x,y,目标资源的宽高w,h,源资源的宽高w,h）
+                imagecopyresampled($canvas, $res, 0, 0, 0, 0, $val['width'], $val['height'],$resWidth,$resHeight);
+                $val['left'] = $val['left']<0?$backgroundWidth- abs($val['left']) - $val['width']:$val['left'];
+                $val['top'] = $val['top']<0?$backgroundHeight- abs($val['top']) - $val['height']:$val['top'];
+                //放置图像
+                imagecopymerge($imageRes,$canvas, $val['left'],$val['top'],$val['right'],$val['bottom'],$val['width'],$val['height'],$val['opacity']);//左，上，右，下，宽度，高度，透明度
+            }
+        }
+
+        //处理文字
+        if(!empty($config['text'])){
+            foreach ($config['text'] as $key => $val) {
+//                $val = array_merge($textDefault,$val);
+                list($R,$G,$B) = explode(',', $val['fontColor']);
+                $fontColor = imagecolorallocate($imageRes, $R, $G, $B);
+                $val['left'] = $val['left']<0?$backgroundWidth- abs($val['left']):$val['left'];
+                $val['top'] = $val['top']<0?$backgroundHeight- abs($val['top']):$val['top'];
+
+                imagettftext($imageRes,$val['fontSize'],$val['angle'],$val['left'],$val['top'],$fontColor,"./wryahei.ttf",$val['text']);
+            }
+        }
+        //生成图片
+        if(!empty($filename)){
+            $res = imagejpeg ($imageRes,$filename,90); //保存到本地
+            imagedestroy($imageRes);
+            if(!$res) return false;
+            return $this->imgToBase64($filename);
+        }else{
+            imagejpeg ($imageRes);     //在浏览器上显示
+            imagedestroy($imageRes);
+        }
+    }
+
+
+    /**
+     * 获取图片的Base64编码(不支持url)
+     * @date 2017-02-20 19:41:22
+     *
+     * @param $img_file 传入本地图片地址
+     *
+     * @return string
+     */
+    function imgToBase64($img_file) {
+
+        $img_base64 = '';
+        if (file_exists($img_file)) {
+            $app_img_file = $img_file; // 图片路径
+            $img_info = getimagesize($app_img_file); // 取得图片的大小，类型等
+
+            //echo '<pre>' . print_r($img_info, true) . '</pre><br>';
+            $fp = fopen($app_img_file, "r"); // 图片是否可读权限
+
+            if ($fp) {
+                $filesize = filesize($app_img_file);
+                $content = fread($fp, $filesize);
+                $file_content = chunk_split(base64_encode($content)); // base64编码
+                switch ($img_info[2]) {           //判读图片类型
+                    case 1: $img_type = "gif";
+                        break;
+                    case 2: $img_type = "jpg";
+                        break;
+                    case 3: $img_type = "png";
+                        break;
+                }
+
+                $img_base64 = 'data:image/' . $img_type . ';base64,' . $file_content;//合成图片的base64编码
+
+            }
+            fclose($fp);
+        }
+
+        return $img_base64; //返回图片的base64
+    }
+
 }
