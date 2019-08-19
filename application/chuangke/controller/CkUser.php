1ed8d4fb9d317d32575ef786f3e49430471f447b
meishuqin <18665056978@163.com>
2019-08-19 17:00:44 +0800

    拒绝流程优化

--- application/chuangke/controller/CkUser.php
+++ application/chuangke/controller/CkUser.php
@@ -417,14 +417,14 @@ class CkUser extends MobileBase
                 }
             }else{
                 if($info['check_leader_1'] == $this->user_id){
-                    if(empty($info['check_leader_2']))  $updata['apply_status'] = $status;
+                    if(empty($info['check_leader_2']) && $status == 1) $updata['apply_status'] = $status;
                     // if(!empty($info['check_status_2'])) $updata['apply_status'] = $status;
                     if($info['check_status_2'] == 1) $updata['apply_status'] = $status;
                     $updata['check_status_1'] = $status;
                     $updata['check_time_1']   = time();
                 }elseif($info['check_leader_2'] == $this->user_id){
                     //第二层领导审核
-                    if(!empty($info['check_status_1']) && $status == 1) $updata['apply_status'] = $status;
+                    if($info['check_status_1'] == 1 && $status == 1) $updata['apply_status'] = $status;
                     $updata['check_status_2'] = $status;
                     $updata['check_time_2']   = time();
                 }else{
